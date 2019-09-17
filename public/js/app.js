7c4f6a5136926dcde08b550c2c054fe7b9e0645e
Victor <adevic4christ@gmail.com>
2019-09-17 10:17:44 +0100

    Add Sign-up functionality

--- public/js/app.js
+++ public/js/app.js
@@ -83,31 +83,102 @@ const UserCtrl = (function () {
 
 //UI Controller
 const UICtrl = (function () {
-    //Selectors
-    // const UISelectors = {
-    //     lname: "#lname",
-    //     fname: "#fname",
-    //     email: "#email",
-    //     password: "#password",
-    //     password1: "#password1",
-    //     dob: "#dob",
-    //     address: "#address",
-    //     occupation: "#occupation",
-    //     income: "#income",
-    //     amount: "#amount",
-    //     submit: "#submit-form"
-    // };
+    // Selectors
+    const UISelectors = {
+        lastName: "#lastName",
+        firstName: "#firstName",
+        email: "#email",
+        gender: "input[name='gender']",
+        password: "#password",
+        password2: "#password2",
+        phone: "phone",
+        dob: "#dob",
+        address: "#address",
+        occupation: "#occupation",
+        houseRent: "houseRent",
+        income: "#income",
+        amount: "#amount",
+        submit: "#submit-form"
+    };
 
-    console.log($(lname).val());
+    // console.log($(UISelectors.gender));
+
+
+    // console.log($(lastName).val());
+
+    //public methods
+    return {
+        getSelectors: function () {
+            return UISelectors;
+        },
+        getUserInput: function () {
+            return {
+                lastName: $(lastName).val(),
+                firstName: $(firstName).val(),
+                email: $(email).val(),
+                gender: $("input[name='gender']:checked").val(),
+                password: $(password).val(),
+                password2: $(password2).val(),
+                phone: $(phone).val(),
+                dob: $(dob).val(),
+                address: $(address).val(),
+                occupation: $(occupation).val(),
+                houseRent: $(houseRent).val(),
+                income: $(income).val(),
+                amount: $(amount).val(),
+            };
+        }
+
+    };
 })();
 
 const App = (function (DbCtrl, UserCtrl, UICtrl) {
+    const UISelectors = UICtrl.getSelectors();
 
+    function loadEventListeners(params) {
+        $(UISelectors.submit).on("click", submitData);
+    }
 
+    const submitData = function (e) {
+        e.preventDefault();
+        //User validation: check if all inputs have value
+        const userValidation = UICtrl.getUserInput();
+        for (const i in userValidation) { // iterate through object
+            if (!userValidation[i] || userValidation[i] === undefined || userValidation[i] === null) {
+                console.log("Enter all fields"); //UI
+                return false;
+            }
+        }
+        //password validation
+        if (userValidation.password !== userValidation.password2) {
+            console.log("Password not matched!");
+            return false;
+        }
+
+        DbCtrl.getData(checkMail);
+    };
+    //make email unique
+    checkMail = function (data) {
+        const currentEmail = UICtrl.getUserInput().email;
+
+        const dbEmail = data.find(el => el.email === currentEmail);
+        if (dbEmail) {
+            console.log("email already exits");
+            return false;
+        }
+
+        const userData = UICtrl.getUserInput();
+
+        if (!dbEmail) {
+            DbCtrl.postData(userData); //make a post request
+        }
+    };
+
+    //public methods
     return {
         init: function () {
             console.log("App initialising...");
-
+            loadEventListeners();
         }
     };
 })(DbCtrl, UserCtrl, UICtrl);
